47fa091f3152e985f7d12d82669478ffa17128d9
mhalpert <mhalpert@gmail.com>
2017-04-17 13:31:50 -0400

    Updated.  Performing much better on AIC for Logistic Regression Model

--- LC Capstone Project.R
+++ LC Capstone Project.R
@@ -9,18 +9,81 @@ library(pROC)
 library(rpart)
 library(randomForest)
 
+### Cleaning up raw data!
+
 RawLoanData <- read.csv("LoanStats3a_securev1.csv", skip = "1", sep = ",", header = TRUE)
-## RawLoanData <- fread("LoanStats3a_securev1.csv", skip = "1", sep = ",", header = TRUE, stringsAsFactors = TRUE)
-## fread.  import as factors
 
-str(RawLoanData)
-summary(RawLoanData)
+## Removing extra fields and NA errors
 
-#Removing extra fields
-
-#Removing columns that are mostly NA
-dropcolumnindex <- lapply(RawLoanData, function(x) sum(is.na(x))) >= 42000
+#Removing columns that are mostly NA's
+dropcolumnindex <- lapply(RawLoanData, function(x) sum(is.na(x))) >= 42300
 RawLoanData <- RawLoanData[ , dropcolumnindex == FALSE]
+
+RawLoanData$int_rate <- as.numeric(gsub("\\%", "", RawLoanData$int_rate))*.01
+RawLoanData$revol_util <- as.numeric(gsub("\\%", "", RawLoanData$revol_util))*.01
+
+
+#Fixing NA's in records with 0's
+RawLoanData$delinq_2yrs[is.na(RawLoanData$delinq_2yrs)] <- 0
+RawLoanData$inq_last_6mths[is.na(RawLoanData$inq_last_6mths)] <- 0
+RawLoanData$open_acc[is.na(RawLoanData$open_acc)] <- 0
+RawLoanData$pub_rec[is.na(RawLoanData$pub_rec)] <- 0
+RawLoanData$total_acc[is.na(RawLoanData$total_acc)] <- 0
+RawLoanData$acc_now_delinq[is.na(RawLoanData$acc_now_delinq)] <- 0
+RawLoanData$mths_since_last_delinq[is.na(RawLoanData$mths_since_last_delinq)] <- 0
+RawLoanData$open_acc[is.na(RawLoanData$open_acc)] <- 0
+RawLoanData$mths_since_last_record[is.na(RawLoanData$mths_since_last_record)] <- 0
+
+
+## Adding new features.  
+
+# Percentage of monthly pay used to pay loan
+RawLoanData$dti <- RawLoanData$dti * 0.01
+RawLoanData$dti_lc <- RawLoanData$installment / (RawLoanData$annual_inc/12)
+RawLoanData$loan_amnt <- NA
+RawLoanData$annual_inc <- NA
+
+# Over 75th percentile for open & active accounts
+#RawLoanData$too_many_accts <-  as.numeric(RawLoanData$open_acc > 12)
+#RawLoanData$open_acc <- NA
+
+# Over 75th percentile for credit inquiries in last 6 months
+#RawLoanData$had_heavy_inq <-  as.numeric(RawLoanData$inq_last_6mths >= 2)
+#RawLoanData$inq_last_6mths <- NA
+
+# Had a delinquincy in the last 2 years
+#RawLoanData$had_delinq <- as.numeric(RawLoanData$delinq_2yrs >= 1)
+#RawLoanData$delinq_2yrs <- NA
+
+# Had a public record
+#RawLoanData$had_pub_rec <- as.numeric(RawLoanData$pub_rec >= 1)
+#RawLoanData$pub_rec <- NA
+
+#Had a bankruptcy
+#RawLoanData$had_pub_rec_bankruptcies <- as.numeric(RawLoanData$pub_rec_bankruptcies >= 1)
+#RawLoanData$pub_rec_bankruptcies <- NA
+
+# Carries a balance above the 75th percentile of borrowers
+#RawLoanData$had_large_bal <- as.numeric(RawLoanData$revol_bal >= 20000)
+#RawLoanData$revol_bal <- NA
+
+# Has a high utilization
+#RawLoanData$had_high_util <- as.numeric(RawLoanData$revol_util >= .75)
+#RawLoanData$revol_util <- NA
+
+## Removing Outcome Fields
+RawLoanData$recoveries <- NA
+RawLoanData$collection_recovery_fee <- NA
+RawLoanData$total_pymnt <- NA
+RawLoanData$total_rec_prncp <- NA
+RawLoanData$total_rec_int <- NA
+RawLoanData$total_rec_late_fee<- NA
+RawLoanData$last_pymnt_amnt <- NA
+RawLoanData$last_fico_range_high <- NA
+RawLoanData$pymnt_plan <- NA
+RawLoanData$acc_now_delinq <- NA
+RawLoanData$delinq_amnt <- NA
+
 
 #Removing text heavy fields
 RawLoanData$url <- NA
@@ -29,7 +92,6 @@ RawLoanData$emp_title <- NA
 RawLoanData$title <- NA
 RawLoanData$application_type <- NA
 RawLoanData$initial_list_status <- NA
-RawLoanData$pymnt_plan <- NA
 RawLoanData$grade <- NA
 
 # Need to figure out how to use the dates more efficiently.  Perhaps as time after loan?
@@ -53,29 +115,14 @@ RawLoanData$total_pymnt_inv <- NA
 RawLoanData$chargeoff_within_12_mths <- NA
 RawLoanData$collections_12_mths_ex_med <- NA
 RawLoanData$tax_liens <- NA
-RawLoanData$revol_util <- as.numeric(RawLoanData$revol_util)
-RawLoanData$int_rate <- as.numeric(RawLoanData$int_rate)
 
-#Creating a credit utilization variable
-#RawLoanData$Credit_Usage <- RawLoanData$revol_util/RawLoanData$revol_bal
 
 
 #Cleaning up empty comment rows and empty income rows
-RawLoanData$annual_inc <- as.numeric(as.character(RawLoanData$annual_inc))
-na_index2 <- which(is.na(RawLoanData$annual_inc))
-RawLoanData <- RawLoanData[-na_index2, ]
+#RawLoanData$annual_inc <- as.numeric(as.character(RawLoanData$annual_inc))
+#na_index2 <- which(is.na(RawLoanData$annual_inc))
+#RawLoanData <- RawLoanData[-na_index2, ]
 
-
-#Fixing NA's in records with 0's
-RawLoanData$delinq_2yrs  <- as.numeric(gsub(NA, 0, RawLoanData$delinq_2yrs))
-RawLoanData$inq_last_6mths  <- as.numeric(gsub(NA, 0, RawLoanData$inq_last_6mths))
-RawLoanData$open_acc  <- as.numeric(gsub(NA, 0, RawLoanData$open_acc))
-RawLoanData$pub_rec  <- as.numeric(gsub(NA, 0, RawLoanData$pub_rec))
-RawLoanData$total_acc  <- as.numeric(gsub(NA, 0, RawLoanData$total_acc))
-RawLoanData$delinq_amnt <- as.numeric(gsub(NA, 0, RawLoanData$delinq_amnt))
-RawLoanData$acc_now_delinq  <- as.numeric(gsub(NA, 0, RawLoanData$acc_now_delinq))
-RawLoanData$mths_since_last_delinq <- as.numeric(gsub(NA, 0, RawLoanData$mths_since_last_delinq))
-RawLoanData$mths_since_last_record <- as.numeric(gsub(NA, 0, RawLoanData$mths_since_last_record))
 
 ## Cleaning up Loan Status
 
@@ -109,7 +156,6 @@ RawLoanData$emp_length <- droplevels(RawLoanData$emp_length)
 RawLoanData$verification_status <- droplevels(RawLoanData$verification_status)
 RawLoanData$purpose <- droplevels(RawLoanData$purpose)
 
-# refactor.  factor function.  Give it levels. 
 # Consolidating defaulted loans.  0 will signify paid back loan.  1 will equal default. 
 
 RawLoanData$loan_status <- as.factor(gsub("Does not meet the credit policy. Status:Fully Paid", "0", RawLoanData$loan_status))
@@ -120,32 +166,27 @@ RawLoanData$loan_status <- as.factor(gsub("Default", "1", RawLoanData$loan_statu
 
 levels(RawLoanData$loan_status)
 
-dropcolumnindex <- lapply(RawLoanData, function(x) sum(is.na(x))) >= 42000
+dropcolumnindex <- lapply(RawLoanData, function(x) sum(is.na(x))) >= 42300
 RawLoanData <- RawLoanData[ , dropcolumnindex == FALSE]
 
-
-#use an if an else
 ## Plotting Loan Status. 
 
 CrossTable(x = RawLoanData$loan_status)
-
 CrossTable(x = RawLoanData$sub_grade, y = RawLoanData$loan_status, prop.r = TRUE, prop.c = FALSE, prop.t = FALSE, prop.chisq = FALSE)
-
 CrossTable(x = RawLoanData$term, y = RawLoanData$loan_status, prop.r = TRUE, prop.c = FALSE, prop.t = FALSE, prop.chisq = FALSE)
-
 CrossTable(x = RawLoanData$home_ownership, y = RawLoanData$loan_status, prop.r = TRUE, prop.c = FALSE, prop.t = FALSE, prop.chisq = FALSE)
-
-
 hist_1 <- hist(RawLoanData$loan_amnt, breaks = 20, xlab = "Loan amount", main = "Histogram of the loan amount")
-
 hist(RawLoanData$fico_range_high, breaks = 50, xlab = "Fico Score", main = "Histogram of the fico score")
-
 plot(RawLoanData$loan_amnt, log(RawLoanData$annual_inc), xlab = "Loan Ammount", ylab = "Annual Income")
-
 ggplot(data.frame(RawLoanData), aes(x=loan_status)) +
   geom_bar()
 
+hist(RawLoanData$revol_bal, breaks = 5, xlab = "Balance", main = "Histogram of the balances")
 
+
+
+
+### Building Predictive Models
 
 ## Splitting a training and testing set
 
@@ -160,91 +201,93 @@ test_set <- RawLoanData[-index_train, ]
 
 ## Building a logistic model.
 
-#log_model_cat <- glm(loan_status ~ loan_amnt+ term + installment + grade + emp_length + annual_inc + purpose +  dti + fico_range_high + inq_last_6mths + mths_since_last_delinq + open_acc + pub_rec + revol_bal + revol_util + total_acc + last_fico_range_high, family= "binomial", data = training_set)
+log_model_all <- glm(loan_status ~ ., family = binomial, data = training_set)
+summary(log_model_all)
+predictions_all <- predict(log_model_all, newdata = test_set, type = "response")
+ROC_all <- roc(test_set$loan_status, predictions_all)
+plot(ROC_all)
+auc(ROC_all)
 
-# Fit the logit, probit and cloglog-link logistic regression models
-# New Factor.  Had a deliquency
+
+# Log Model 1.  Removing unneccesary variables. 
+
+# All = log_model_logit <- glm(loan_status ~  loan_amnt + term + int_rate + installment + sub_grade + emp_length + home_ownership + annual_inc + verification_status + purpose +  dti + fico_range_high + mths_since_last_delinq + mths_since_last_record + total_acc + dti_lc + too_many_accts + had_heavy_inq + had_delinq + had_pub_rec + had_pub_rec_bankruptcies + had_large_bal + had_high_util, family = binomial(link = logit) , data = training_set)
+
+#logit model
+#log_model_logit <- glm(loan_status ~  term + installment + sub_grade + emp_length + home_ownership + purpose + fico_range_high + dti_lc + had_heavy_inq + had_pub_rec + had_large_bal + had_high_util, family = binomial(link = logit) , data = training_set)
+# number 2 - log_model_logit <- glm(loan_status ~  loan_amnt + term + int_rate + installment + sub_grade + emp_length + home_ownership + annual_inc + verification_status + purpose + dti + delinq_2yrs + fico_range_high + inq_last_6mths + mths_since_last_delinq + mths_since_last_record + open_acc + pub_rec + revol_bal + revol_util + total_acc + pub_rec_bankruptcies + dti_lc, family = binomial(link = logit) , data = training_set)
+log_model_logit <- glm(loan_status ~  term + installment + sub_grade + emp_length + home_ownership + purpose + fico_range_high + inq_last_6mths + mths_since_last_record + revol_bal + revol_util + dti_lc, family = binomial(link = logit) , data = training_set)
+summary(log_model_logit)
+predictions_logit <- predict(log_model_logit, newdata = test_set, type = "response")
+ROC_Logit <- roc(test_set$loan_status, predictions_logit)
+plot(ROC_Logit)
+auc(ROC_Logit)
+
+#probit model
+#log_model_probit <- glm(loan_status ~  term + installment + sub_grade + emp_length + home_ownership + purpose +  dti + fico_range_high + dti_lc + had_heavy_inq + had_pub_rec + had_large_bal + had_high_util, family = binomial(link = probit) , data = training_set)
+log_model_probit <- glm(loan_status ~   term + installment + sub_grade + emp_length + home_ownership + purpose + fico_range_high + inq_last_6mths + mths_since_last_record + revol_bal + revol_util + dti_lc, family = binomial(link = probit) , data = training_set)
+summary(log_model_probit)
+predictions_probit <- predict(log_model_probit, newdata = test_set, type = "response")
+ROC_probit <- roc(test_set$loan_status, predictions_probit)
+plot(ROC_probit)
+auc(ROC_probit)
+
+#log_model_cloglog <- glm(loan_status ~  term + installment + sub_grade + emp_length + home_ownership + purpose +  dti + fico_range_high + dti_lc + had_heavy_inq + had_pub_rec + had_large_bal + had_high_util, family = binomial(link = cloglog) , data = training_set)
+log_model_cloglog <- glm(loan_status ~   term + installment + sub_grade + emp_length + home_ownership + purpose + fico_range_high + inq_last_6mths + mths_since_last_record + revol_bal + revol_util + dti_lc, family = binomial(link = cloglog) , data = training_set)
+summary(log_model_cloglog)
+predictions_cloglog <- predict(log_model_cloglog, newdata = test_set, type = "response")
+ROC_cloglog <- roc(test_set$loan_status, predictions_cloglog)
+plot(ROC_probit)
+auc(ROC_probit)
 
 ## corrplot
 
 
-log_model_logit <- glm(loan_status ~ loan_amnt + term + int_rate + installment + sub_grade + home_ownership + annual_inc + purpose +  verification_status + dti + fico_range_high + pub_rec_bankruptcies , family = binomial(link = logit) , data = training_set)
-log_model_logit2 <- glm(loan_status ~ term + sub_grade + home_ownership + annual_inc + purpose + dti + fico_range_high + pub_rec_bankruptcies , family = binomial(link = logit) , data = training_set)
-
-#old_log_model_logit2 <- glm(loan_status ~ term + sub_grade + annual_inc + purpose + dti + pub_rec_bankruptcies , family = binomial(link = logit) , data = training_set)
-
-log_model_all <- glm(loan_status ~ ., family = binomial(link = probit), data = training_set)
-
-
-
-summary(log_model_logit)
-summary(log_model_logit2)
-summary(log_model_all)
-
-## Not sure why error happens here 
-# log_model_probit <- glm(loan_status ~ loan_amnt+ term + installment + grade + emp_length + annual_inc + purpose +  dti + fico_range_high ,family = binomial(link = probit), data = training_set)
-# log_model_cloglog <- glm(loan_status ~ loan_amnt+ term + installment + grade + emp_length + annual_inc + purpose +  dti + fico_range_high ,family = binomial(link = cloglog), data = training_set)
-
-# Make predictions for all models using the test set
-predictions_logit <- predict(log_model_logit, newdata = test_set, type = "response")
-predictions_logit2 <- predict(log_model_logit2, newdata = test_set, type = "response")
-predictions_logitall <- predict(log_model_all, newdata = test_set, type = "response")
-predictions_probit <- predict(log_model_probit, newdata = test_set, type = "response")
-predictions_cloglog <- predict(log_model_cloglog, newdata = test_set, type = "response")
-
-# Assesing model with ROC Curve
-ROC_Logit <- roc(test_set$loan_status, predictions_logit)
-ROC_Logit2 <- roc(test_set$loan_status, predictions_logit2)
-ROC_LogitAll <- roc(test_set$loan_status, predictions_logitall)
-
-plot(ROC_Logit)
-auc(ROC_Logit)
-plot(ROC_Logit2)
-auc(ROC_Logit2)
-plot(ROC_LogitAll)
-auc(ROC_LogitAll)
 
 
 
 # Use a cut-off of 30% to make binary predictions-vectors
-cutoff <- 0.27
+cutoff <- 0.30
+class_pred_all <- ifelse(predictions_all > cutoff, 1, 0)
 class_pred_logit <- ifelse(predictions_logit > cutoff, 1, 0)
-class_pred_logit2 <- ifelse(predictions_logit2 > cutoff, 1, 0)
 class_pred_probit <- ifelse(predictions_probit > cutoff, 1, 0)
 class_pred_cloglog <- ifelse(predictions_cloglog > cutoff, 1, 0)
 
 # Make a confusion matrix for the three models
+tab_class_all <- table(test_set$loan_status,class_pred_all)
 tab_class_logit <- table(test_set$loan_status,class_pred_logit)
-tab_class_logit2 <- table(test_set$loan_status,class_pred_logit2)
-tab_class_probit <- table(true_val,class_pred_probit)
-tab_class_cloglog <- table(true_val,class_pred_cloglog)
+tab_class_probit <- table(test_set$loan_status,class_pred_probit)
+tab_class_cloglog <- table(test_set$loan_status,class_pred_cloglog)
 
 # Compute the classification accuracy for all three models
+acc_all <- sum(diag(tab_class_all)) / nrow(test_set)
 acc_logit <- sum(diag(tab_class_logit)) / nrow(test_set)
-acc_logit2 <- sum(diag(tab_class_logit2)) / nrow(test_set)
 acc_probit <- sum(diag(tab_class_probit)) / nrow(test_set)
 acc_cloglog <- sum(diag(tab_class_cloglog)) / nrow(test_set)
 
 #print reults
+acc_all
 acc_logit
-acc_logit2
 acc_probit
 acc_cloglog
 
+tab_class_all
 tab_class_logit
-tab_class_logit2
+tab_class_probit
+tab_class_cloglog
+
 
 ## Prediction Trees
 
 # Creating the first base classification tree. 
-tree1 <- rpart(loan_status ~ term + sub_grade + annual_inc + purpose + dti, method = "class", data =  training_set, control = rpart.control(cp = 0.001))
+tree1 <- rpart(loan_status ~ term + int_rate + installment + sub_grade + emp_length + home_ownership +  verification_status + purpose + dti + delinq_2yrs + fico_range_high + inq_last_6mths + mths_since_last_delinq + mths_since_last_record + open_acc + pub_rec + revol_bal + revol_util + total_acc + pub_rec_bankruptcies + dti_lc, method = "class", data =  training_set, control = rpart.control(cp = 0.001))
 
 # Plot the decision tree
 plot(tree1, uniform = TRUE)
 text(tree1)
 
 #Changing Weights
-tree_prior <- rpart(loan_status ~ ., method = "class",data = training_set, parms = list(prior=c(0.7, 0.3)), control = rpart.control(cp = 0.001))
+tree_prior <- rpart(loan_status ~ term + int_rate + installment + sub_grade + emp_length + home_ownership, method = "class",data = training_set, parms = list(prior=c(0.7, 0.3)), control = rpart.control(cp = 0.001))
 
 # Plot the decision tree
 plot(tree_prior, uniform = TRUE)
@@ -260,6 +303,11 @@ printcp(tree1)
 
 
 ## Random Forests
+
+fit_all <- randomForest(as.factor(loan_status) ~ .,
+                    data=training_set, 
+                    importance=TRUE, 
+                    ntree=2000)
 
 fit <- randomForest(as.factor(loan_status) ~ term + sub_grade + annual_inc + purpose + dti + pub_rec,
                     data=training_set, 
